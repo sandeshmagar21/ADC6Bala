8d5bb0eabc3926598adac257a599bc9ba30c92eb
sandeshmagar21 <58873487+sandeshmagar21@users.noreply.github.com>
2019-12-16 11:25:28 +0545

    update third line again

--- firstFileBala.py
+++ firstFileBala.py
@@ -1,3 +1,3 @@
 print("Hello from the other side, from tthe universe")
 print("Hello from the other side,hello sathi haru like and subscribe na gara pne hunxa hai")
-print("Hello solti haru")
+print("Hello solti haru, dhoka deis gada")
